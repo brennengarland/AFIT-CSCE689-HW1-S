456107faef7f09b6f888bdaade24653cc68994ab
brennen-homeserver <bgarland015@gmail.com>
2020-01-10 01:03:00 +0000

    pushing server code

--- src/TCPServer.cpp
+++ src/TCPServer.cpp
@@ -1,4 +1,8 @@
 #include "TCPServer.h"
+#include <sys/socket.h>
+#include <stdexcept>
+#include <netinet/in.h> 
+
 
 TCPServer::TCPServer() {
 
@@ -16,8 +20,31 @@ TCPServer::~TCPServer() {
  *    Throws: socket_error for recoverable errors, runtime_error for unrecoverable types
  **********************************************************************************************/
 
-void TCPServer::bindSvr(const char *ip_addr, short unsigned int port) {
+void TCPServer::bindSvr(const char *ip_addr, short unsigned int port) 
+{
+    int server_sock;
+    struct sockaddr_in address;
 
+    if((server_sock = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        throw std::invalid_argument("runtime_error"); //TO-DO: Fix error handling
+    }
+
+    int opt = 1;
+    if(setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        throw std::invalid_argument("runtime_error");
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( 8080 );
+
+    if(bind(server_sock, (struct  sockaddr *)&address, sizeof(address))<0)
+    {
+        throw std::invalid_argument("runtime_error");
+    }
+    
    
 }
 
